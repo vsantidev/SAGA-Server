49caff4c6ed5b93053b3c56134f1961372fc1c87
vsantidev <vsantidev@gmail.com>
2024-01-27 20:35:49 +0100

    AuthController : login et register + route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminaite\Support\Facade\Log;
+use Illuminate\Support\Facades\Log as FacadesLog;
+
+use function Laravel\Prompts\info;
 
 class AuthController extends Controller
 {
@@ -41,18 +46,35 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $request->validate([
-            'email'=> 'required|string|email|',
-            'password' => 'required|string',
-        ]);
 
-        if(!Auth::attempt($request->only('email','password'))) {
-            return response()->json(['message' => 'Invalid detail login'], 401);
+        $user = User::where('email', $request->email)->first();
+
+        echo($user);
+        if ($user && Hash::check($request->password, $user->password)) {
+            // Utilisation de Sanctum pour créer un jeton d'accès
+            $token = $user->createToken(time())->plainTextToken;
+
+            return response()->json([
+                'token' => $token,
+                'user' => $user, 
+            ]);
+        } else {
+            return response()->json(['error' => 'Email ou mot de passe incorrect'], 401);
         }
 
-        $user = $request->user();
-        $token = $user->createToken('authToken')->plainTextToken;
-        return response()->json(['user' => $user, 'token' => $token]);
+
+        // $request->validate([
+        //     'email'=> 'required|string|email|',
+        //     'password' => 'required|string',
+        // ]);
+
+        // if(!Auth::attempt($request->only('email','password'))) {
+        //     return response()->json(['message' => 'Invalid detail login'], 401);
+        // }
+
+        // $user = $request->user();
+        // $token = $user->createToken('authToken')->plainTextToken;
+        // return response()->json(['user' => $user, 'token' => $token]);
     }
 
     public function logout(Request $request)
--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'home', 'login', 'user'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
