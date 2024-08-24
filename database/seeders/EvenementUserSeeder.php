8968f89a8afa19e1359d2cd5dab25270c3d90c65
jswyv <jsowyvern@gmail.com>
2024-08-24 23:58:02 +0200

    seeder userconv

--- database/seeders/EvenementUserSeeder.php
+++ database/seeders/EvenementUserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Evenement_user;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,44 @@ class EvenementUserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Evenement_user::create([
+            'evenement_id' => '1',
+            'user_id'  => '1',
+        ]);
+
+        Evenement_user::create([
+            'evenement_id' => '1',
+            'user_id'  => '2',
+        ]);
+
+        Evenement_user::create([
+            'evenement_id' => '1',
+            'user_id'  => '3',
+        ]);
+
+        Evenement_user::create([
+            'evenement_id' => '1',
+            'user_id'  => '4',
+        ]);
+
+        Evenement_user::create([
+            'evenement_id' => '1',
+            'user_id'  => '5',
+        ]);
+
+        Evenement_user::create([
+            'evenement_id' => '1',
+            'user_id'  => '6',
+        ]);
+
+        Evenement_user::create([
+            'evenement_id' => '1',
+            'user_id'  => '7',
+        ]);
+
+        Evenement_user::create([
+            'evenement_id' => '1',
+            'user_id'  => '8',
+        ]);
     }
 }
