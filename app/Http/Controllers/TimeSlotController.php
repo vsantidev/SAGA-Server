d8d48b07bae4488b084be4c532e7a3b5e6f3bf5a
jswyv <jsowyvern@gmail.com>
2026-09-11 23:13:08 +0200

    rollback correctif

--- app/Http/Controllers/TimeSlotController.php
+++ app/Http/Controllers/TimeSlotController.php
@@ -180,17 +180,6 @@ class TimeSlotController extends Controller
             }
         }
 
-        // Deuxième passe : convertir les rejected en cascade_cancelled
-        // si l'user a finalement été placé ailleurs
-        foreach ($inscriptions as $inscription) {
-            if (
-                $inscription->status === 'rejected' &&
-                isset($usersPlaced[$inscription->user_id])
-            ) {
-                $inscription->update(['status' => 'cascade_cancelled']);
-            }
-        }
-
         $timeSlot->update([
             'draw_status' => 'drawn',
             'drawn_at'    => now(),
